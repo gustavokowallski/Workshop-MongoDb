d63e9e4b4285151c8f2c5d230c7269095ca12dd2
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-09-04 16:24:18 -0300

    feat(User): Add User entity and UserRepository

---
+++ mongodb/src/main/java/com/workshop/mongodb/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.workshop.mongodb.repositories;
+
+import com.workshop.mongodb.models.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
