228856e55be475d175fcab6a986e62f83d881c11
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-10-01 15:44:41 -0300

    feat: add GET Post by title, query operators

--- mongodb/src/main/java/com/workshop/mongodb/services/PostService.java
+++ mongodb/src/main/java/com/workshop/mongodb/services/PostService.java
@@ -28,7 +28,7 @@ public class PostService {
     }
 
     public List<PostDTO> findByTitle(String text){
-        List<Post> posts = postRepository.findByTitleContainingIgnoreCase(text);
+        List<Post> posts = postRepository.searchTitle(text);
         return posts.stream().map(PostDTO::new).toList();
     }
 
